bd4953089fe052336f1e95e512bf1c34fd914150
Owen Griffiths <root@ogriffiths.com>
2022-05-06 20:05:04 +0100

    Capacity initialisation is max is defined

--- src/cache.rs
+++ src/cache.rs
@@ -17,26 +17,28 @@ impl<K: Eq + Hash + Clone, V> Cache<K, V> {
     /// Initializes new cache with optional `max` length
     pub fn new(max: impl Into<Option<usize>>) -> Self {
         info!("Initializing cache");
-
-        // Create cache
         let max = max.into();
-        let mut cache = Self {
-            inner: HashMap::new(),
-            old: vec![],
-            max,
-        };
-
-        // Add capacity to old
-        if let Some(old_cap) = cache.prune_over() {
-            cache.old = Vec::with_capacity(old_cap);
+        if max.is_some() {
+            // Fix capacity for initial perf if max is defined
+            Self {
+                inner: HashMap::with_capacity(prune_len(max).unwrap()),
+                old: Vec::with_capacity(prune_over(max).unwrap()),
+                max,
+            }
+        } else {
+            // No max is defined, don't add initial capacity
+            Self {
+                inner: HashMap::new(),
+                old: vec![],
+                max,
+            }
         }
-        cache
     }
 
     /// Inserts new item into cache
     pub fn insert(&mut self, k: K, v: V) -> Option<V> {
         // Check if it should be added to `old`
-        match self.prune_over() {
+        match prune_over(self.max) {
             Some(over) if self.old.len() < over => self.old.push(k.clone()),
             _ => (),
         }
@@ -49,8 +51,9 @@ impl<K: Eq + Hash + Clone, V> Cache<K, V> {
     /// Prunes cache to [Self::max] if it's 20% over
     fn prune(&mut self) {
         trace!("Checking if cache needs pruning");
-        match self.prune_len() {
+        match prune_len(self.max) {
             Some(len) if self.inner.len() > len => {
+                // TODO: capacity isnt reducing
                 trace!("Pruning cache because it's 20% over");
                 for k in &self.old {
                     self.inner.remove(k);
@@ -60,16 +63,16 @@ impl<K: Eq + Hash + Clone, V> Cache<K, V> {
             _ => (),
         }
     }
+}
 
-    /// Value (if any) defined which causes the cache to be pruned
-    fn prune_len(&self) -> Option<usize> {
-        self.max.map(|max| max + self.prune_over().unwrap())
-    }
+/// Value (if any) defined which causes the cache to be pruned
+fn prune_len(max: Option<usize>) -> Option<usize> {
+    max.map(|val| val + prune_over(max).unwrap())
+}
 
-    /// Amount maximum is allowed to be over, used for [Self::prune_len] calculations
-    fn prune_over(&self) -> Option<usize> {
-        self.max.map(|max| (max as f64 * 0.2) as usize)
-    }
+/// Amount maximum is allowed to be over, used for [Self::prune_len] calculations
+fn prune_over(max: Option<usize>) -> Option<usize> {
+    max.map(|val| (val as f64 * 0.2) as usize)
 }
 
 #[cfg(test)]
@@ -83,5 +86,6 @@ mod tests {
         for i in 0..500000 {
             cache.insert(i, i * 2);
         }
+        assert_eq!(cache.inner.capacity(), 1200)
     }
 }
