5e8d42087d5ee094d942fa6a1e3c900eff5382e1
Owen Griffiths <root@ogriffiths.com>
2022-05-13 12:47:55 +0100

    Ping-pong action

--- src/message/action.rs
+++ src/message/action.rs
@@ -1,9 +1,22 @@
 /// Intention of any given message
-pub enum Action {}
+pub enum Action {
+    /// Allows peers to test connections to each other
+    ///
+    /// # About
+    ///
+    /// One peer sends an unsolicited ping-pong request another, and the receiving peer simply sends the ping-pong response back. This involves no data transfer but still uses encryption to test the full cycle works. The flow is the following:
+    ///
+    /// 1. Peer A sends this request to Peer B
+    /// 2. Peer B decodes the request and sends back a ping-pong response
+    /// 3. Peer A knows it can connect to Peer B if the response can be decrypted
+    PingPong,
+}
 
 impl From<Action> for u8 {
     fn from(action: Action) -> Self {
-        match action {}
+        match action {
+            Action::PingPong => 0,
+        }
     }
 }
 
